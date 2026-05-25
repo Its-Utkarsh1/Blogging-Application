e050c6979c4951f588ae0ef34059512f35c13410
Utkarsh <utkarsh.sri.9170@gmail.com>
2026-05-25 23:56:50 +0530

    disabled security temporarily

--- src/main/java/com/blogging/Security/SecurityConfig.java
+++ src/main/java/com/blogging/Security/SecurityConfig.java
@@ -2,71 +2,37 @@ package com.blogging.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import java.nio.file.AccessDeniedException;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig{
-
-    private JwtAuthenticationEntryPoint authenticationEntryPoint;
-    private JtwAuthenticationFilter jwtAuthenticationFilter;
-
-    public SecurityConfig(JwtAuthenticationEntryPoint authenticationEntryPoint, JtwAuthenticationFilter authenticationFilter) {
-        this.authenticationEntryPoint = authenticationEntryPoint;
-        this.jwtAuthenticationFilter = authenticationFilter;
-    }
+public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception  {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                        .authorizeHttpRequests(req -> req
-                                .requestMatchers(HttpMethod.POST,"/api/v1/auth/login").permitAll()
-                                .requestMatchers(HttpMethod.POST,"/api/v1/auth/signup").permitAll()
-                                .requestMatchers(HttpMethod.DELETE,"/api/v1/users/**","/api/v1/category/**").hasRole("ADMIN")
-                                .requestMatchers(HttpMethod.GET,"/api/v1/**").permitAll()
-                                .anyRequest().authenticated()
-                        );
-
-
-        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        http.exceptionHandling(exception -> exception.authenticationEntryPoint(authenticationEntryPoint)
-                .accessDeniedHandler((request, response, accessDeniedException) ->{
-                    response.setStatus(HttpStatus.FORBIDDEN.value());
-                    response.getWriter().write("{ \"error\": \"Access Denied\"}");
-                }));
-        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                .authorizeHttpRequests(req ->
+                        req.anyRequest().permitAll()
+                )
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
 
         return http.build();
     }
 
-
-
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration
+    ) throws Exception {
         return configuration.getAuthenticationManager();
     }
-
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
-
-
-
-
 }
